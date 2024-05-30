1d5c43ab934aa875835d6aae7f12874c109f71ad
Juan Ignacio <128737896+ChunchoMetalero@users.noreply.github.com>
2024-05-30 14:07:58 -0400

    Tarea 4 Terminada y 5

---
+++ T5/rwlock.c
@@ -0,0 +1,93 @@
+#define _XOPEN_SOURCE 500
+
+#include "nthread-impl.h"
+
+#include "rwlock.h"
+
+struct rwlock {
+  NthQueue *queue_readers;
+  NthQueue *queue_writters;
+  int readers;
+  int writters;
+
+};
+
+nRWLock *nMakeRWLock() {
+  nRWLock *rwl = malloc(sizeof(nRWLock));
+  rwl->queue_readers = nth_makeQueue();
+  rwl->queue_writters = nth_makeQueue();
+  rwl->readers = 0;
+  rwl->writters = 0;
+  return rwl;
+}
+
+void nDestroyRWLock(nRWLock *rwl) {
+  nth_destroyQueue(rwl->queue_readers);
+  nth_destroyQueue(rwl->queue_writters);
+  free(rwl);
+}
+
+int nEnterRead(nRWLock *rwl, int timeout) {
+  START_CRITICAL
+  if (rwl->writters == 0 && nth_emptyQueue(rwl->queue_writters)){
+    rwl->readers++;
+  }
+  else{
+    nThread this_th = nSelf();
+    nth_putBack(rwl->queue_readers, this_th);
+    suspend(WAIT_RWLOCK);
+    schedule();
+  }
+  END_CRITICAL
+  return 1;
+}
+
+int nEnterWrite(nRWLock *rwl, int timeout) {
+  START_CRITICAL
+  if ((rwl->writters == 0) && (rwl->readers == 0)){
+    rwl->writters++;
+  }
+  else{
+    nThread this_th = nSelf();
+    nth_putBack(rwl->queue_writters, this_th);
+    suspend(WAIT_RWLOCK);
+    schedule();
+  }
+  END_CRITICAL
+  return 1;
+}
+
+void nExitRead(nRWLock *rwl) {
+  START_CRITICAL
+
+  rwl->readers--;
+
+  if(rwl->readers == 0 && !nth_emptyQueue(rwl->queue_writters)){
+    nThread th = nth_getFront(rwl->queue_writters);
+    setReady(th);
+    rwl->writters++;
+  }
+
+  END_CRITICAL
+}
+
+void nExitWrite(nRWLock *rwl) {
+  START_CRITICAL
+
+  rwl->writters--;
+
+  if (!nth_emptyQueue(rwl->queue_readers)){
+    for (int i = 0; i <= nth_queueLength(rwl->queue_readers); i++){
+      nThread th = nth_getFront(rwl->queue_readers);
+      setReady(th);
+      rwl->readers++;
+    }
+  }
+  else if(nth_emptyQueue(rwl->queue_readers) && !nth_emptyQueue(rwl->queue_writters)){
+    nThread th = nth_getFront(rwl->queue_writters);
+    setReady(th);
+    rwl->writters++;
+  }
+
+  END_CRITICAL
+}
